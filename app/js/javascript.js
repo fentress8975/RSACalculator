c3c52fcaaa1893a7c6c6543f43d9522c33591d88
fentress8975 <troshkinss@mail.ru>
2024-12-22 21:01:10 +0400

    связь между front и back

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -1,51 +1,57 @@
-function calculateEulAndN() {
-    const p = document.getElementById("p").value;
-    const q = document.getElementById("q").value;
-    const n = document.getElementById("n").value;
-    const eul = document.getElementById("eul").value;
-    const url = "http://localhost/calculateEulAndN" + "?" + new URLSearchParams({ p: p, q: q });
-    const json = getData(url);
+async function calculateEulAndN() {
+    const p = document.getElementById("p");
+    const q = document.getElementById("q");
+    const n = document.getElementById("n");
+    const eul = document.getElementById("eul");
+    const url = "http://localhost/calculateEulAndN" + "?" + new URLSearchParams({ p: p.value, q: q.value });
+    const data = await getData(url);
+    eul.value = data.eul;
+    n.value = data.n;
 }
 
-function calculateE() {
-    const eul = document.getElementById("eul").value;
-    const url = "http://localhost/calculateE" + "?" + new URLSearchParams({ eul: eul });
-    const json = getData(url);
+async function calculateE() {
+    const eul = document.getElementById("eul");
+    const e = document.getElementById("e");
+    const url = "http://localhost/calculateE" + "?" + new URLSearchParams({ eul: eul.value });
+    const data = await getData(url);
+    e.value = data.e;
 }
 
-function calculateD() {
-    const eul = document.getElementById("eul").value;
-    const e = document.getElementById("e").value;
-    const d = document.getElementById("d").value;
-    const url = "http://localhost/calculateD" + "?" + new URLSearchParams({ eul: eul, e: e });
-    const json = getData(url);
+async function calculateD() {
+    const eul = document.getElementById("eul");
+    const e = document.getElementById("e");
+    const d = document.getElementById("d");
+    const url = "http://localhost/calculateD" + "?" + new URLSearchParams({ eul: eul.value, e: e.value });
+    const data = await getData(url);
+    d.value = data.d;
 }
 
-function encryptM() {
-    const c = document.getElementById("c").value;
-    const m = document.getElementById("m").value;
-    const url = "http://localhost/encryptM" + "?" + new URLSearchParams({ c: c, m: m });
-    const json = getData(url);
+async function encryptM() {
+    const c = document.getElementById("c");
+    const m = document.getElementById("m");
+    const url = "http://localhost/encryptM" + "?" + new URLSearchParams({ c: c.value, m: m.value });
+    const data = await getData(url);
+    c.value = data.c;
 }
 
-function decryptC() {
-    const c = document.getElementById("c").value;
-    const m = document.getElementById("m").value;
-    const url = "http://localhost/decryptC" + "?" + new URLSearchParams({ c: c, m: m });
-    const json = getData(url);
+async function decryptC() {
+    const c = document.getElementById("c");
+    const m = document.getElementById("m");
+    const url = "http://localhost/decryptC" + "?" + new URLSearchParams({ c: c.value, m: m.value });
+    const data = await getData(url);
+    m.value = data.m;
 }
 
 async function getData(url) {
     try {
         const response = await fetch(url);
         if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`);
+            throw new Error(`Response status: ${response.status}`);
         }
-    
-        const json = await response.json();
-        console.log(json);
-        return(json);
-      } catch (error) {
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
         console.error(error.message);
-      }
+    }
 }
--- app/php/route.php
+++ app/php/route.php
@@ -1,30 +1,37 @@
 <?php
 
 $action = explode("?", $_SERVER["REQUEST_URI"]);
+header('Content-Type: application/json; charset=UTF-8');
 
 switch ($action[0]) {
     case '/calculateEulAndN':
-        echo $_SERVER["QUERY_STRING"];
+
+        $testarr = ['eul' => 101, 'n' => 202];
+        echo json_encode($testarr);
         die();
         break;
     case '/calculateE':
-        echo $_SERVER["QUERY_STRING"];
+        $testarr = ['e' => 101000];
+        echo json_encode($testarr);
         die();
         break;
     case '/calculateD':
-        echo $_SERVER["QUERY_STRING"];
+        $testarr = ['d' => 303003];
+        echo json_encode($testarr);
         die();
         break;
     case '/encryptM':
-        echo $_SERVER["QUERY_STRING"];
+        $testarr = ['c' => 21321312];
+        echo json_encode($testarr);
         die();
         break;
     case '/decryptC':
-        echo $_SERVER["QUERY_STRING"];
+        $testarr = ['m' => 1018989898];
+        echo json_encode($testarr);
         die();
         break;
 
     default:
-        
+        header('Content-Type: text/html; charset=UTF-8');
         break;
 }
