4b26df866d96d367c6e41f967245278fff642b2d
fentress8975 <troshkinss@mail.ru>
2024-12-23 19:25:51 +0400

    проверка что передано число

--- app/php/route.php
+++ app/php/route.php
@@ -5,10 +5,23 @@ $action = explode("?", $_SERVER["REQUEST_URI"]);
 header('Content-Type: application/json; charset=UTF-8');
 include_once("php/RSACalculator.php");
 
+//простая проверка на число
+function isNumber(...$variable): bool
+{
+    foreach ($variable as $value) {
+        if (!is_numeric($value)) {
+            echo json_encode(['error' => 'Получено не число']);
+            die();
+        }
+    }
+    return true;
+}
+isNumber(...$_GET);
+//
+
 switch ($action[0]) {
     case '/calculateEulAndN':
-
-        echo json_encode(RSACalculator::calculateEulAndN($_GET['q'],$_GET['p']));
+        echo json_encode(RSACalculator::calculateEulAndN($_GET['q'], $_GET['p']));
         die();
         break;
     case '/calculateE':
@@ -16,15 +29,15 @@ switch ($action[0]) {
         die();
         break;
     case '/calculateD':
-        echo json_encode(RSACalculator::calculateD($_GET['e'],$_GET['eul'],));
+        echo json_encode(RSACalculator::calculateD($_GET['e'], $_GET['eul'],));
         die();
         break;
     case '/encryptM':
-        echo json_encode(RSACalculator::encryptM($_GET['m'],$_GET['n'],$_GET['e']));
+        echo json_encode(RSACalculator::encryptM($_GET['m'], $_GET['n'], $_GET['e']));
         die();
         break;
     case '/decryptC':
-        echo json_encode(RSACalculator::decryptC($_GET['c'],$_GET['d'],$_GET['n']));
+        echo json_encode(RSACalculator::decryptC($_GET['c'], $_GET['d'], $_GET['n']));
         die();
         break;
 
