39b3345ab2610c9d50c21a0740ac3451b23d124f
fentress8975 <troshkinss@mail.ru>
2024-12-23 19:26:09 +0400

    вывод ошибок на странице

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -1,49 +1,106 @@
+const errors_p = {
+    error_EulAndN: document.getElementsByClassName("EulAndN-error")[0],
+    error_e: document.getElementsByClassName("e-error")[0],
+    error_d: document.getElementsByClassName("d-error")[0],
+    error_cAndM: document.getElementsByClassName("cAndM-error")[0],
+};
+
+function clearField() {
+    const inputs = [];
+    inputs.push(document.getElementById("p"),
+        document.getElementById("q"),
+        document.getElementById("n"),
+        document.getElementById("eul"),
+        document.getElementById("e"),
+        document.getElementById("d"),
+        document.getElementById("c"),
+        document.getElementById("m")
+    );
+
+    inputs.forEach(element => {
+        element.value = '';
+    });
+    clearErrors();
+}
+
 async function calculateEulAndN() {
+    clearErrors();
     const p = document.getElementById("p");
     const q = document.getElementById("q");
     const n = document.getElementById("n");
     const eul = document.getElementById("eul");
     const url = "http://localhost/calculateEulAndN" + "?" + new URLSearchParams({ p: p.value, q: q.value });
     const data = await getData(url);
-    eul.value = data.eul;
-    n.value = data.n;
+    if (Object.keys(data).includes("error")) {
+        showError(data, errors_p.error_EulAndN);
+    }
+    else {
+        eul.value = data.eul;
+        n.value = data.n;
+    }
+
 }
 
 async function calculateE() {
+    clearErrors();
     const eul = document.getElementById("eul");
     const e = document.getElementById("e");
     const url = "http://localhost/calculateE" + "?" + new URLSearchParams({ eul: eul.value });
     const data = await getData(url);
-    e.value = data.e;
+
+    if (Object.keys(data).includes("error")) {
+        showError(data, errors_p.error_e);
+    }
+    else {
+        e.value = data.e;
+    }
 }
 
 async function calculateD() {
+    clearErrors();
     const eul = document.getElementById("eul");
     const e = document.getElementById("e");
     const d = document.getElementById("d");
     const url = "http://localhost/calculateD" + "?" + new URLSearchParams({ eul: eul.value, e: e.value });
     const data = await getData(url);
-    d.value = data.d;
+    if (Object.keys(data).includes("error")) {
+        showError(data, errors_p.error_d);
+    }
+    else {
+        d.value = data.d;
+    }
 }
 
 async function encryptM() {
+    clearErrors();
     const c = document.getElementById("c");
     const m = document.getElementById("m");
     const e = document.getElementById("e");
     const n = document.getElementById("n");
     const url = "http://localhost/encryptM" + "?" + new URLSearchParams({ m: m.value, e: e.value, n: n.value });
     const data = await getData(url);
-    c.value = data.c;
+    if (Object.keys(data).includes("error")) {
+        showError(data, errors_p.error_cAndM);
+    }
+    else {
+        c.value = data.c;
+    }
 }
 
 async function decryptC() {
+    clearErrors();
     const c = document.getElementById("c");
     const m = document.getElementById("m");
     const d = document.getElementById("d");
     const n = document.getElementById("n");
     const url = "http://localhost/decryptC" + "?" + new URLSearchParams({ c: c.value, d: d.value, n: n.value });
     const data = await getData(url);
-    m.value = data.m;
+    if (Object.keys(data).includes("error")) {
+        showError(data, errors_p.error_cAndM);
+    }
+    else {
+        m.value = data.m;
+    }
 }
 
 async function getData(url) {
@@ -57,5 +114,16 @@ async function getData(url) {
         return result;
     } catch (error) {
         console.error(error.message);
+    }
+}
+
+function showError(data, p) {
+    clearErrors();
+    p.innerText = data.error;
+}
+
+function clearErrors() {
+    for (const [key, value] of Object.entries(errors_p)) {
+        value.innerText = '';
     }
 }
